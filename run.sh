2a3122b19f458522cf9b970dab615fb7c6934515
Aseem Ratha <aseemratha@gmail.com>
2024-11-12 12:47:29 -0500

    first commit

---
+++ run.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+# Check for the OS type
+if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
+    # Windows with MinGW
+    cmake . -B build -G "MinGW Makefiles"
+else
+    # Unix-based system (macOS/Linux)
+    cmake . -B build -G "Unix Makefiles"
+fi
+cmake --build build
+build/art-game
