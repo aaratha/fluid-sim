83941f9e352775e65cbfe537bdd6fc9729687e85
Aseem Ratha <aseemratha@gmail.com>
2024-11-27 20:08:16 -0500

    emscripten web port (unstable)

--- run.sh
+++ run.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env sh
 
 # Check for the OS type
-if [[ "$OSTYPE" == "msys" || "$OSTYPE" == "win32" ]]; then
-    # Windows with MinGW
-    cmake . -B build -G "MinGW Makefiles"
+
+if [ "$1" = "web" ]; then
+    emcmake cmake . -B build-web -DPLATFORM=Web -DCMAKE_BUILD_TYPE=Release
+    emmake make -C build-web
+    python -m http.server
 else
-    # Unix-based system (macOS/Linux)
-    cmake . -B build -G "Unix Makefiles"
+    if [ "$OSTYPE" = "msys" ] || [ "$OSTYPE" = "win32" ]; then
+        # Windows with MinGW
+        cmake . -B build -G "MinGW Makefiles"
+    else
+        # Unix-based system (macOS/Linux)
+        cmake . -B build -G "Unix Makefiles"
+    fi
+    cmake --build build
+    build/fluid-sim
 fi
-cmake --build build
-build/fluid-sim
